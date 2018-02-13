f0f3966605133201cfedf4053e31119b214a9c6a
luc-girod <luc.girod@gmail.com>
2018-02-13 16:52:40 +0100

    Addec counter to DevAllPrep

--- DevAllPrep.sh
+++ DevAllPrep.sh
@@ -1,12 +1,13 @@
 #Script to force single core convertion off all images into RGB tifs (somethime AperiCloud fails on machines with many cores, trying to convert too many images at the same time)
-
+nbIm=0
 mkdir Tmp-MM-Dir
 find ./ -maxdepth 1 -regex ".*\(JPG\|jpg\|png\|PNG\|ARW\|NEF\|CR2\)" | while read file;
 do
+	nbIm=$((nbIm + 1)) 
     f=$(basename "$file")
     #echo "mm3d MpDcraw \"./$f\"  Add16B8B=0  ConsCol=0  ExtensionAbs=None  16B=0  CB=1  NameOut=./Tmp-MM-Dir/"$f"_Ch3.tif Gamma=2.2 EpsLog=1.0" >> DevAll.sh
 	echo "mm3d Devlop \"./$f\" 8B=1 Gray=0 NameOut=./Tmp-MM-Dir/"$f"_Ch3.tif" >> DevAll.sh
 	echo "Found image : " $f
 done  
-
+echo "Found " $nbIm " images."
 bash DevAll.sh
